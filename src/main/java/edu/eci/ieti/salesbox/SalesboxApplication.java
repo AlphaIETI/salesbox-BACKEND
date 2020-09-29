56580fb3fd57bd406c9386d38701c59f57cf8385
JuanNavarroJ <Juan.navarro@mail.escuelaing.edu.co>
2020-09-28 19:13:10 -0500

    Estructura del proyecto creada entre todos

---
+++ src/main/java/edu/eci/ieti/salesbox/SalesboxApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.salesbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/ieti/salesbox/SalesboxApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.salesbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
