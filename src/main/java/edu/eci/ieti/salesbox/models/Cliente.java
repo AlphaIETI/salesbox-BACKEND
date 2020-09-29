c86950dc8ab105bdd39fe080319820a6888b398f
checho1998 <c.hecho.28@hotmail.com>
2020-09-29 13:26:04 -0500

    cliente

---
+++ src/main/java/edu/eci/ieti/salesbox/models/Cliente.java
@@ -0,0 +1,59 @@
+package edu.eci.ieti.salesbox.models;
+
+public class Cliente {
+
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contraseña;
+
+    public Cliente(int id, String nombre, String apellido, String correo, String contraseña) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
