97556d9e043bc33cd53e856d8c14e40c1d41d2e6
checho1998 <c.hecho.28@hotmail.com>
2020-09-29 23:55:02 -0500

    arreglo

--- src/main/java/edu/eci/ieti/salesbox/models/Cliente.java
+++ src/main/java/edu/eci/ieti/salesbox/models/Cliente.java
@@ -9,51 +9,72 @@ public class Cliente {
     private String contrase;
 
     public Cliente(int id, String nombre, String apellido, String correo, String contrase) {
+
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.correo = correo;
         this.contrase = contrase;
+
     }
 
-
     public int getId() {
+
         return id;
+
     }
 
     public String getNombre() {
+
         return nombre;
+
     }
 
     public String getApellido() {
+
         return apellido;
+
     }
 
     public String getCorreo() {
+
         return correo;
+
     }
 
     public String getContrase() {
+
         return contrase;
+
     }
 
     public void setId(int id) {
+
         this.id = id;
+
     }
 
     public void setNombre(String nombre) {
+
         this.nombre = nombre;
+
     }
 
     public void setApellido(String apellido) {
+
         this.apellido = apellido;
+
     }
 
     public void setCorreo(String correo) {
+
         this.correo = correo;
+
     }
 
     public void setContraseña(String contrase) {
+
         this.contrase = contrase;
+
     }
 }
