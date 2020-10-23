17d06f1cb2cfca33f7f1a4cabc5b9492e2cd7301
checho1998 <c.hecho.28@hotmail.com>
2020-10-22 19:42:37 -0500

    mas de cliente

--- src/main/java/edu/eci/ieti/salesbox/models/Client.java
+++ src/main/java/edu/eci/ieti/salesbox/models/Client.java
@@ -12,74 +12,84 @@ public class Client {
     private String lastname;
     private String mail;
     private String password;
+    private String coupons;
+    private String phon;
+    private String adress;
 
-    public Client(int id, String name, String lastname, String mail, String password) {
+    public Client(int id, String name, String lastname, String mail, String password,String coupons,String phon, String adress ) {
 
         this.id = id;
         this.name = name;
         this.lastname = lastname;
         this.mail = mail;
         this.password = password;
+        this.coupons = coupons;
+        this.phon = phon;
+        this.adress = adress;
 
     }
 
     public int getId() {
-
         return id;
-
     }
 
-    public String getname() {
-
+    public String getName() {
         return name;
-
     }
 
-    public String getlastname() {
-
+    public String getLastname() {
         return lastname;
-
     }
 
-    public String getmail() {
-
+    public String getMail() {
         return mail;
-
     }
 
-    public String getpassword() {
-
+    public String getPassword() {
         return password;
+    }
 
+    public String getCoupons() {
+        return coupons;
+    }
+
+    public String getPhon() {
+        return phon;
+    }
+
+    public String getAdress() {
+        return adress;
     }
 
     public void setId(int id) {
-
         this.id = id;
-
     }
 
-    public void setname(String name) {
-
+    public void setName(String name) {
         this.name = name;
-
     }
 
-    public void setlastname(String lastname) {
-
+    public void setLastname(String lastname) {
         this.lastname = lastname;
-
     }
 
-    public void setmail(String mail) {
-
+    public void setMail(String mail) {
         this.mail = mail;
-
     }
 
-    public void setpassword(String password) {
-
+    public void setPassword(String password) {
         this.password = password;
+    }
 
+    public void setCoupons(String coupons) {
+        this.coupons = coupons;
+    }
+
+    public void setPhon(String phon) {
+        this.phon = phon;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
     }
 }
