7af45c606748f6ae0cd33c8a2db6047b128d5280
JuanNavarroJ <Juan.navarro@mail.escuelaing.edu.co>
2020-10-28 14:08:56 -0500

    comentarios

---
+++ src/main/java/edu/eci/ieti/salesbox/exceptions/PromotionException.java
@@ -0,0 +1,9 @@
+package edu.eci.ieti.salesbox.exceptions;
+
+public class PromotionException extends Exception{
+
+    public PromotionException (String message){
+        super(message);
+    }
+
+}
