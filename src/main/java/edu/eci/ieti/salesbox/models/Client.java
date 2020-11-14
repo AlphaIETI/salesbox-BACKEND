984643c6dbea9f6dec4cf1f83b6870d1690d803a
julianagarzond <juliana.garzon-d@mail.escuelaing.edu.co>
2020-11-14 14:22:10 -0500

    gender

--- src/main/java/edu/eci/ieti/salesbox/models/Client.java
+++ src/main/java/edu/eci/ieti/salesbox/models/Client.java
@@ -21,11 +21,14 @@ public class Client {
     private String sizeUp;
     private String sizeDown;
     private String shoeSize;
+    private String gender;
     private ArrayList<String> cart = new ArrayList<>();
     private ArrayList<String> favorites = new ArrayList<>();
 
 
-    public Client(String id, String name, String lastname, String email, String password, String coupons, String phone, String address, String age, String sizeUp, String sizeDown, String shoeSize, ArrayList<String> cart, ArrayList<String> favorites) {
+
+
+    public Client(String id, String name, String lastname, String email, String password, String coupons, String phone, String address, String age, String sizeUp, String sizeDown, String shoeSize, String gender, ArrayList<String> cart, ArrayList<String> favorites) {
         this.id = id;
         this.name = name;
         this.lastname = lastname;
@@ -38,6 +41,7 @@ public class Client {
         this.sizeUp = sizeUp;
         this.sizeDown = sizeDown;
         this.shoeSize = shoeSize;
+        this.gender= gender;
         this.cart = cart;
         this.favorites = favorites;
     }
@@ -185,6 +189,8 @@ public class Client {
     public void setFavorites(ArrayList<String> favorites) {
         this.favorites = favorites;
     }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
 
     @Override
     public String toString() {
