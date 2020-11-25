1ba2fdae4ca1d9c368beb831b181c1eb6cdd7b45
camilavieda04 <camila_vieda04@hotmail.com>
2020-11-24 21:51:56 -0500

    Coupon attributes

--- src/main/java/edu/eci/ieti/salesbox/controllers/CouponController.java
+++ src/main/java/edu/eci/ieti/salesbox/controllers/CouponController.java
@@ -3,7 +3,7 @@ import edu.eci.ieti.salesbox.models.Coupon;
 import edu.eci.ieti.salesbox.services.impl.CouponServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import java.time.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,7 +33,12 @@ public class CouponController {
     @PostMapping("/coupons")
     public Coupon createCoupon(@RequestBody Coupon newCoupon) {
         UUID uuid=UUID.randomUUID();
+        LocalDate ld = LocalDate.now(); 
+        LocalDate end = ld.plusDays(10); 
+        int percentage = (int)(Math.random()*(30-10+1)+10);
         newCoupon.setId(uuid.toString());
+        newCoupon.setEndDate(end.toString());
+        newCoupon.setPercentage(percentage);
         return couponService.createCoupon(newCoupon);
     }
 
