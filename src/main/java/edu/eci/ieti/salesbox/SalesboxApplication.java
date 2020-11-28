6a6e4b0587e57ce2595ef0e4c2be45cf4bfb4a84
DavidDiaz1949 <david.diaz-d@mail.escuelaing.edu.co>
2020-11-28 14:09:01 -0500

    CROSS ADD

--- src/main/java/edu/eci/ieti/salesbox/SalesboxApplication.java
+++ src/main/java/edu/eci/ieti/salesbox/SalesboxApplication.java
@@ -2,11 +2,25 @@ package edu.eci.ieti.salesbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class SalesboxApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SalesboxApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("https://salesbox-alpha.herokuapp.com/");
+			}
+		};
 	}
 
 }
